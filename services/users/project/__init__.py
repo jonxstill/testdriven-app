4bb45360a17ae541be5b5dadd67feac24d068a82
Jon Still <jrs-git@jonstill.co.uk>
2019-02-07 23:06:33 +0000

    Added docker support

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,14 @@
+import os
+import sys
 from flask import Flask, jsonify
 
 # Instantiate the app
 app = Flask(__name__)
 
 # Set Config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
